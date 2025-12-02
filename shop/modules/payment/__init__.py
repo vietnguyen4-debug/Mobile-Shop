5bcfb866b51b5216b55cf3c655d3443fd6dd0391
vietnguyen4-debug <vietnguyen4@interspace.vn>
2025-12-02 14:49:42 +0700

    Minor fixes

--- shop/modules/payment/__init__.py
+++ shop/modules/payment/__init__.py
@@ -1,4 +1,4 @@
 from flask import Blueprint
 
-bp = Blueprint('payments', __name__, url_prefix='/api/payments')
-bp_admin = Blueprint('payments', __name__, url_prefix='/api/admin/payments')
+bp = Blueprint("payments", __name__, url_prefix="/api/payments")
+bp_admin = Blueprint("payments_admin", __name__, url_prefix="/api/admin/payments")
